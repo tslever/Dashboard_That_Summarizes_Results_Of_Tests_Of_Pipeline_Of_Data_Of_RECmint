7edbe84c7f96b84859d4f9bd6120ea833ddd3906
Tom Lever <thomas.lever.business@gmail.com>
2024-08-16 13:52:00 -0400

    Defined path to data

--- Loader.py
+++ Loader.py
@@ -6,9 +6,13 @@ import pandas as pd
 class Loader():
 
 
+    def __init__(self):
+        self.path_to_data = "C:/Users/Tom/Documents/RecMint/data/"
+
+
     def count_columns_of_table_Generators_of_database_AirTable(self):
         maximum_number_of_keys = 0
-        with open("C:/Users/Tom/Documents/RecMint/data/AirTable/Generators.ndjson", 'r') as file:
+        with open(self.path_to_data + "AirTable/Generators.ndjson", 'r') as file:
             for line in file:
                 JSON_object = json.loads(line)
                 number_of_keys = len(JSON_object.keys())
@@ -19,7 +23,7 @@ class Loader():
 
     def count_columns_of_table_Generators_of_database_RECBus(self):
         data_frame = pd.read_csv(
-            filepath_or_buffer = "C:/Users/Tom/Documents/RecMint/data/RECBus/Generators.csv",
+            filepath_or_buffer = self.path_to_data + "RECBus/Generators.csv",
             header = 0,
             nrows = 1
         )
@@ -29,7 +33,7 @@ class Loader():
 
     def count_rows_of_table_Generators_of_database_AirTable(self):
         count = 0
-        with open("C:/Users/Tom/Documents/RecMint/data/AirTable/Generators.ndjson", 'r') as file:
+        with open(self.path_to_data + "AirTable/Generators.ndjson", 'r') as file:
             for line in file:
                 count += 1
         return count
@@ -37,7 +41,7 @@ class Loader():
 
     def count_rows_of_table_Generators_of_database_AirTable_without_key_sysID(self):
         count = 0
-        with open("C:/Users/Tom/Documents/RecMint/data/AirTable/Generators.ndjson", 'r') as file:
+        with open(self.path_to_data + "AirTable/Generators.ndjson", 'r') as file:
             for line in file:
                 JSON_object = json.loads(line)
                 if "sysID" not in JSON_object:
@@ -47,7 +51,7 @@ class Loader():
 
     def count_rows_of_table_Generators_of_database_RECBus(self):
         data_frame = pd.read_csv(
-            filepath_or_buffer = "C:/Users/Tom/Documents/RecMint/data/RECBus/Generators.csv",
+            filepath_or_buffer = self.path_to_data + "RECBus/Generators.csv",
             header = 0
         )
         count = len(data_frame.index)
@@ -56,7 +60,7 @@ class Loader():
 
     def count_rows_of_table_Generators_of_database_RECBus_without_key_sysid(self):
         data_frame = pd.read_csv(
-            filepath_or_buffer = "C:/Users/Tom/Documents/RecMint/data/RECBus/Generators.csv",
+            filepath_or_buffer = self.path_to_data + "RECBus/Generators.csv",
             header = 0,
             nrows = 1
         )
@@ -66,7 +70,7 @@ class Loader():
 
     def create_excerpt_of_table_Generators_of_database_AirTable(self):
         data = []
-        with open("C:/Users/Tom/Documents/RecMint/data/AirTable/Generators.ndjson", 'r') as file:
+        with open(self.path_to_data + "AirTable/Generators.ndjson", 'r') as file:
             for i, line in enumerate(file):
                 if i == 1:
                     break
@@ -78,7 +82,7 @@ class Loader():
 
     def create_excerpt_of_table_Generators_of_database_RECBus(self):
         data_frame = pd.read_csv(
-            filepath_or_buffer = "C:/Users/Tom/Documents/RecMint/data/RECBus/Generators.csv",
+            filepath_or_buffer = self.path_to_data + "RECBus/Generators.csv",
             header = 0,
             nrows = 1
         )
@@ -88,7 +92,7 @@ class Loader():
 
     def create_list_of_values_in_column_Nameplate_of_table_Generators_of_AirTable(self) -> list:
         list_of_values_in_column_nameplate_of_table_Generators_of_AirTable = []
-        with open(f"C:/Users/Tom/Documents/RecMint/data/AirTable/Generators.ndjson", 'r') as file:
+        with open(self.path_to_data + "AirTable/Generators.ndjson", 'r') as file:
             data = ndjson.load(file)
             for record in data:
                 if "Nameplate (kW DC)" in record:
@@ -100,7 +104,7 @@ class Loader():
 
     def create_list_of_values_in_column_nominal_power_of_table_Generators_of_RECBus(self) -> list:
         data_frame = pd.read_csv(
-            filepath_or_buffer = "C:/Users/Tom/Documents/RecMint/data/RECBus/Generators.csv",
+            filepath_or_buffer = self.path_to_data + "RECBus/Generators.csv",
             header = 0
         )
         return data_frame["nominal-power"].to_list()
