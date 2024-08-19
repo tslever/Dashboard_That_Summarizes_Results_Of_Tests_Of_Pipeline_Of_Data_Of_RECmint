16a66c5a314d8bb5060eaaa0ac26eb23a89e985a
Tom Lever <thomas.lever.business@gmail.com>
2024-08-19 15:06:28 -0400

    table of statistics of table Generators is a data table

--- dashboard/tables/table_of_statistics_of_table_Generators.py
+++ dashboard/tables/table_of_statistics_of_table_Generators.py
@@ -1,25 +1,41 @@
-from dashboard.rows.row_with_counts_of_columns_of_table_Generators \
-    import row_with_counts_of_columns_of_table_Generators
-from dashboard.rows.row_with_counts_of_rows_in_table_Generators \
-    import row_with_counts_of_rows_in_table_Generators
-from dashboard.rows.row_with_counts_of_rows_in_table_Generators_missing_system_ID \
-    import row_with_counts_of_rows_missing_system_ID
-from dashboard.rows.row_with_label_Statistics_For_Database import row_with_label_Statistics_For_Database
-from dashboard.rows.row_with_numbers_of_generators_in_database_that_do_not_correspond_to_generators_in_other_database \
-    import row_with_numbers_of_generators_in_database_that_do_not_correspond_to_generators_in_other_database
-from dashboard.Table import Table
+from dash import dash_table
+from Loader import loader
 
 
-list_of_rows = [
-    row_with_label_Statistics_For_Database,
-    row_with_counts_of_columns_of_table_Generators,
-    row_with_counts_of_rows_in_table_Generators,
-    row_with_counts_of_rows_missing_system_ID,
-    row_with_numbers_of_generators_in_database_that_do_not_correspond_to_generators_in_other_database
+list_of_columns = [
+    {"name": "Description Of Statistic For AirTable", "id": "Description Of Statistic For AirTable"},
+    {"name": "Value Of Statistic For AirTable", "id": "Value Of Statistic For AirTable"},
+    {"name": "Description Of Statistic For RECBus", "id": "Description Of Statistic For RECBus"},
+    {"name": "Value Of Statistic For RECBus", "id": "Value Of Statistic For RECBus"}
 ]
 
-
-table_of_statistics_of_table_Generators = Table(
-    children = list_of_rows,
-    width = 100
+table_of_statistics_of_table_Generators = dash_table.DataTable(
+    columns = list_of_columns,
+    data = [
+        {
+            "Description Of Statistic For AirTable": "count of columns in table Generators",
+            "Value Of Statistic For AirTable": loader.count_columns_of_table_Generators_of_database_AirTable(),
+            "Description Of Statistic For RECBus": "count of columns in table Generators",
+            "Value Of Statistic For RECBus": loader.count_columns_of_table_Generators_of_database_RECBus(),
+        },
+        {
+            "Description Of Statistic For AirTable": "count of rows in table Generators",
+            "Value Of Statistic For AirTable": loader.count_rows_of_table_Generators_of_database_AirTable(),
+            "Description Of Statistic For RECBus": "count of rows in table Generators",
+            "Value Of Statistic For RECBus": loader.count_rows_of_table_Generators_of_database_RECBus(),
+        },
+        {
+            "Description Of Statistic For AirTable": "count of rows in table Generators without key sysID",
+            "Value Of Statistic For AirTable": loader.count_rows_of_table_Generators_of_database_AirTable_without_key_sysID(),
+            "Description Of Statistic For RECBus": "count of rows in table Generators without key sysid",
+            "Value Of Statistic For RECBus": loader.count_rows_of_table_Generators_of_database_RECBus_without_key_sysid(),
+        },
+        {
+            "Description Of Statistic For AirTable": "count of rows in table Generators without key sysID",
+            "Value Of Statistic For AirTable": loader.calculate_number_of_generators_in_AirTable_that_do_not_correspond_to_generators_in_RECBus(),
+            "Description Of Statistic For RECBus": "count of rows in table Generators without key sysid",
+            "Value Of Statistic For RECBus": loader.calculate_number_of_generators_in_RECBus_that_do_not_correspond_to_generators_in_AirTable(),
+        }
+    ],
+    style_table = {"width": "100%"}
 )
