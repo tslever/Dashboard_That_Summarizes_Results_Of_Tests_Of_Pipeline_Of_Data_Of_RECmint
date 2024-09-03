b1b47db162bba49b5a1d873ee81e7d86dcd49c34
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-02 22:24:57 -0400

    Dynamically generated graph of modeled Y1 RECs

--- dashboard/rows/row_with_graphs_of_frequency_of_Modeled_Y1_RECs_vs_Modeled_Y1_RECs.py
+++ dashboard/rows/row_with_graphs_of_frequency_of_Modeled_Y1_RECs_vs_Modeled_Y1_RECs.py
@@ -1,16 +1,15 @@
 from dashboard.Cell import Cell
 from dash import html
-from dashboard.graphs.graph_of_frequency_of_Modeled_Y1_RECs_vs_Modeled_Y1_RECs_per_table_Generators_of_AirTable import \
-    graph_of_frequency_of_Modeled_Y1_RECs_vs_Modeled_Y1_RECs_per_table_Generators_of_AirTable
+from dashboard.Grapher import grapher
 
 
 list_of_children = [
     Cell(
-        graph_of_frequency_of_Modeled_Y1_RECs_vs_Modeled_Y1_RECs_per_table_Generators_of_AirTable,
+        grapher.graph_frequency_of_quantity_vs_quantity(name_of_database = "AirTable", quantity = "Modeled Y1 RECs", log_should_be_applied = True),
         width = 50
     ),
     Cell(
-        [],
+        None,
         width = 50
     )
 ]
