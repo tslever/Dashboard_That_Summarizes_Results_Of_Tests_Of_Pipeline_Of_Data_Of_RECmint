cacfcc2f67bc759d3ba131b533f529ed5a0b10ba
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-03 13:46:42 -0400

    Dynamically generated row with graph of specific yield

--- dashboard/tables/table_of_visualizations_of_frequency_of_Specific_Yield_of_table_Generators.py
+++ dashboard/tables/table_of_visualizations_of_frequency_of_Specific_Yield_of_table_Generators.py
@@ -1,10 +1,13 @@
-from dashboard.rows.row_with_graphs_of_frequency_of_Specific_Yield_vs_Yield import \
-    row_with_graphs_of_frequency_of_Specific_Yield_vs_Yield
+from dashboard.Creator_Of_Row import creator_of_row
 from dashboard.Table import Table
 
 
 list_of_rows = [
-    row_with_graphs_of_frequency_of_Specific_Yield_vs_Yield
+    creator_of_row.create_row_of_graphs_of_frequency_of_quantity_vs_quantity(
+        quantity_in_AirTable = "Specific Yield (given)",
+        quantity_in_RECBus = None,
+        log_should_be_applied = False
+    )
 ]
 
 
