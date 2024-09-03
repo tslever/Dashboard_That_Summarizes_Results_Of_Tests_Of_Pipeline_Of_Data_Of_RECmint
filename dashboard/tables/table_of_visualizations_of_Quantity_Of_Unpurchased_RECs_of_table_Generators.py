17228d969e86db7137e1b475a839b90320c337f9
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-03 13:44:58 -0400

    Dynamically generated row with graph of quantity of unpurchased RECs

--- dashboard/tables/table_of_visualizations_of_Quantity_Of_Unpurchased_RECs_of_table_Generators.py
+++ dashboard/tables/table_of_visualizations_of_Quantity_Of_Unpurchased_RECs_of_table_Generators.py
@@ -1,10 +1,13 @@
-from dashboard.rows.row_with_graphs_of_frequency_of_Quantity_Of_Unpurchased_RECs_vs_Quantity \
-    import row_with_graphs_of_frequency_of_Quantity_Of_Unpurchased_RECs_vs_Quantity
+from dashboard.Creator_Of_Row import creator_of_row
 from dashboard.Table import Table
 
 
 list_of_rows = [
-    row_with_graphs_of_frequency_of_Quantity_Of_Unpurchased_RECs_vs_Quantity
+    creator_of_row.create_row_of_graphs_of_frequency_of_quantity_vs_quantity(
+        quantity_in_AirTable = "Qty Unpurchased RECs",
+        quantity_in_RECBus = None,
+        log_should_be_applied = True
+    )
 ]
 
 
