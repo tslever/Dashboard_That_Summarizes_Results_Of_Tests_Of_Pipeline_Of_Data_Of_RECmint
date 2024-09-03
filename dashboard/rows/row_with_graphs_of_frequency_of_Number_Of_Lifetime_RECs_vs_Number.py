e5f94df5ef1fd4e58aeda97caa826dbc5e5de57f
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-02 22:34:12 -0400

    Dynamically generated graph of number of lifetime RECs

--- dashboard/rows/row_with_graphs_of_frequency_of_Number_Of_Lifetime_RECs_vs_Number.py
+++ dashboard/rows/row_with_graphs_of_frequency_of_Number_Of_Lifetime_RECs_vs_Number.py
@@ -1,16 +1,15 @@
 from dashboard.Cell import Cell
 from dash import html
-from dashboard.graphs.graph_of_frequency_of_Number_Of_Lifetime_RECs_vs_Number_per_table_Generators_of_AirTable import \
-    graph_of_frequency_of_Number_Of_Lifetime_RECs_vs_Number_per_table_Generators_of_AirTable
+from dashboard.Grapher import grapher
 
 
 list_of_children = [
     Cell(
-        graph_of_frequency_of_Number_Of_Lifetime_RECs_vs_Number_per_table_Generators_of_AirTable,
+        grapher.graph_frequency_of_quantity_vs_quantity(name_of_database = "AirTable", quantity = "Lifetime RECs", log_should_be_applied = True),
         width = 50
     ),
     Cell(
-        [],
+        None,
         width = 50
     )
 ]
