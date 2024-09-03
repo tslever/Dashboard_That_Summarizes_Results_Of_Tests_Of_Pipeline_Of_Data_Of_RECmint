46675b41e8fbe59f15072c865ff0ec365a21aff9
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-03 13:30:18 -0400

    Dynamically generated row with graph of MS

--- dashboard/tables/table_of_visualizations_of_MS_of_table_Generators.py
+++ dashboard/tables/table_of_visualizations_of_MS_of_table_Generators.py
@@ -1,9 +1,13 @@
-from dashboard.rows.row_with_graphs_of_frequency_of_MS_vs_MS import row_with_graphs_of_frequency_of_MS_vs_MS
+from dashboard.Creator_Of_Row import creator_of_row
 from dashboard.Table import Table
 
 
 list_of_rows = [
-    row_with_graphs_of_frequency_of_MS_vs_MS
+    creator_of_row.create_row_of_graphs_of_frequency_of_quantity_vs_quantity(
+        quantity_in_AirTable = "M&S - Fee%",
+        quantity_in_RECBus = None,
+        log_should_be_applied = False
+    )
 ]
 
 
