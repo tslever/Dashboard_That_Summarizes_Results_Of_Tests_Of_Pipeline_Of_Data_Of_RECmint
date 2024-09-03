c3674a0aa742836c09d20304d4feba751a066790
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-02 22:36:58 -0400

    Dynamically generated graph of number of mismatches

--- dashboard/rows/row_with_graphs_of_frequency_of_Number_Of_Mismatches_vs_Number.py
+++ dashboard/rows/row_with_graphs_of_frequency_of_Number_Of_Mismatches_vs_Number.py
@@ -1,16 +1,15 @@
 from dashboard.Cell import Cell
 from dash import html
-from dashboard.graphs.graph_of_frequency_of_Number_Of_Mismatches_vs_Number_per_table_Generators_of_AirTable import \
-    graph_of_frequency_of_Number_Of_Mismatches_vs_Number_per_table_Generators_of_AirTable
+from dashboard.Grapher import grapher
 
 
 list_of_children = [
     Cell(
-        graph_of_frequency_of_Number_Of_Mismatches_vs_Number_per_table_Generators_of_AirTable,
+        grapher.graph_frequency_of_quantity_vs_quantity(name_of_database = "AirTable", quantity = "TEMP - GenSync row mismatch (expected RECs", log_should_be_applied = False),
         width = 50
     ),
     Cell(
-        [],
+        None,
         width = 50
     )
 ]
