1419d9a4bd9edb4a5834bfa945e435be1cab2d95
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-03 13:48:13 -0400

    Dynamically generated row with graph of STT1 revenue

--- dashboard/tables/table_of_visualizations_of_frequency_of_STT1_Revenue_of_table_Generators.py
+++ dashboard/tables/table_of_visualizations_of_frequency_of_STT1_Revenue_of_table_Generators.py
@@ -1,10 +1,13 @@
-from dashboard.rows.row_with_graphs_of_frequency_of_STT1_Revenue_vs_Revenue import \
-    row_with_graphs_of_frequency_of_STT1_Revenue_vs_Revenue
+from dashboard.Creator_Of_Row import creator_of_row
 from dashboard.Table import Table
 
 
 list_of_rows = [
-    row_with_graphs_of_frequency_of_STT1_Revenue_vs_Revenue
+    creator_of_row.create_row_of_graphs_of_frequency_of_quantity_vs_quantity(
+        quantity_in_AirTable = "STT1 Revenue",
+        quantity_in_RECBus = None,
+        log_should_be_applied = False
+    )
 ]
 
 
