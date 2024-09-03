b6d683a7003f80e22c45551d9ed31ca13fe790e9
Tom Lever <46533664+tslever@users.noreply.github.com>
2024-09-03 13:24:24 -0400

    Dynamically generated row with graph of count of contracts

--- dashboard/tables/table_of_visualizations_of_Count_Of_Contracts_of_table_Generators.py
+++ dashboard/tables/table_of_visualizations_of_Count_Of_Contracts_of_table_Generators.py
@@ -1,10 +1,13 @@
-from dashboard.rows.row_with_graphs_of_frequency_of_Count_Of_Contracts_vs_Count \
-    import row_with_graphs_of_frequency_of_Count_Of_Contracts_vs_Count
+from dashboard.Creator_Of_Row import creator_of_row
 from dashboard.Table import Table
 
 
 list_of_rows = [
-    row_with_graphs_of_frequency_of_Count_Of_Contracts_vs_Count
+    creator_of_row.create_row_of_graphs_of_frequency_of_quantity_vs_quantity(
+        quantity_in_AirTable = "COUNT - Contracts",
+        quantity_in_RECBus = None,
+        log_should_be_applied = False
+    )
 ]
 
 
